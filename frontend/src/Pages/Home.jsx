ff203f653a972dd55f98331e2da744bbe8ae7004
Cole J Keller <colejkeller7@gmail.com>
2026-08-02 10:13:28 -0500

    Added new comp into HomePage

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 
 //Learn useStates and useEffects
 
+import ToDoList from "../components/ToDoList";
+
 function Home()
 {
     return <div>
     <h1> This is the home page!</h1>
+    <ToDoList></ToDoList>
              </div>
 }
 
