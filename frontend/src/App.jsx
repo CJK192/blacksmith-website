76716928c42a13b66fd10f94241de3efe06e35c8
joshualobert <joshualobert10@gmail.com>
2026-08-02 17:27:17 -0500

    added cart
    added cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar'
 import Home from './Pages/Home'
 import Support from './Pages/Support'
 import About from './Pages/About'
+import Cart from './Pages/Cart'
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App()
     <Route path = '/' element={<Home />}/>
     <Route path = '/support' element={<Support />}/>
     <Route path = '/about' element={<About />}/>
+    <Route path = '/cart' element={<Cart />}/>
   </Routes>
 
   </div>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ function NavBar()
        <Link to ='/'><li> Home</li> </Link>
         <Link to ='/about'><li> About</li> </Link>
         <Link to ='/support'><li> Support</li> </Link>
+        <Link to ='/cart'><li> Cart</li> </Link>
         
     </ul>
 }
